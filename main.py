52530cfac6529a21067d217b03983521bc7dae9c
Obaada <159489903+Obaada@users.noreply.github.com>
2024-02-16 01:06:28 +0000

    hurdle 2 & 3

--- main.py
+++ main.py
@@ -25,3 +25,46 @@ print(subtract(4,5))
     #   turn_left()
     # for x in range(0,6):
     #   jump()
+########################################################################
+# Hurdle 2
+    # def turn_right():
+    #   turn_left()
+    #   turn_left()
+    #   turn_left()
+    # def jump():
+    #   move()
+    #   turn_left()
+    #   move()
+    #   turn_right()
+    #   move()
+    #   turn_right()
+    #   move()
+    #   turn_left()
+    # #for x in range(0,6):
+    # #    jump()
+    # number_of_hurdles = 6
+    # while number_of_hurdles > 0:
+    #   jump()
+    #   number_of_hurdles -= 1
+    #   if at_goal():
+    #       done()
+########################################################################
+# Hurdle 3
+    # def turn_right():
+    #   turn_left()
+    #   turn_left()
+    #   turn_left()
+    # def jump():
+    # #   move()
+    #   turn_left()
+    #   move()
+    #   turn_right()
+    #   move()
+    #   turn_right()
+    #   move()
+    #   turn_left()
+    # while not at_goal():
+    #   if wall_in_front():
+    #       jump()
+    #   if not at_goal() and front_is_clear():
+    #       move()
